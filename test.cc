3eea1d883a765ececce3e812df4bedd7f7af5350
Shriram V <shriam.vishwanathan@foghorn.io>
2018-11-07 12:01:40 +0530

    Minor update

--- test.cc
+++ test.cc
@@ -8,68 +8,68 @@ using namespace shri314::util;
 int main() {
    using AM = active_map_t<int, int>;
    
-   AM wcm;
+   AM test_object;
 
    {
       std::unique_ptr<AM::handle_t> h2;
 
       {
-         assert( wcm.size() == 0 );
+         assert( test_object.size() == 0 );
 
-         auto&& h1 = wcm.put(10, 100);
+         auto&& h1 = test_object.put(10, 100);
 
-         wcm.put(10, 101);
-         wcm.put(11, 110);
+         test_object.put(10, 101);
+         test_object.put(11, 110);
 
-         assert( wcm.size() == 1 );
+         assert( test_object.size() == 1 );
 
          h2.reset(new AM::handle_t(h1));
       }
 
-      assert( wcm.size() == 1 );
+      assert( test_object.size() == 1 );
       assert( h2 );
       assert( h2->value() == 101 );
 
       {
-         auto ha = wcm.get(10);
+         auto ha = test_object.get(10);
          assert( ha );
          assert( ha.value() == 101 );
       }
 
       {
-         auto ha = wcm.get(11);
+         auto ha = test_object.get(11);
          assert( !ha );
       }
    }
 
    {
-      auto ha = wcm.get(10);
+      auto ha = test_object.get(10);
       assert( !ha );
    }
 
-   assert( wcm.size() == 0 );
+   assert( test_object.size() == 0 );
 
    {
-      auto hx = wcm.put(12, 120);
-      auto hy = wcm.put(13, 130);
+      auto hx = test_object.put(12, 120);
+      auto hy = test_object.put(13, 130);
 
-      assert( wcm.size() == 2 );
+      assert( test_object.size() == 2 );
 
       int s = 0;
-      wcm.for_each([&s](auto& k, auto& v) {
+      test_object.for_each([&s](auto& k, auto& v) {
          s += v++;
       });
 
       assert(s == 250);
 
       s = 0;
-      wcm.for_each([&s](auto& k, auto& v) {
+      test_object.for_each([&s](auto& k, auto& v) {
          s += v;
       });
 
       assert(s == 252);
-      assert( wcm.size() == 2 );
+      assert( test_object.size() == 2 );
    }
 
-   assert( wcm.size() == 0 );
+   assert( test_object.size() == 0 );
 }
